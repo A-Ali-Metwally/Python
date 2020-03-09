46b4654eddb9f88c4119af14c2533888f17c6846
Ahmed Metwally <31157155+A-Ali-Metwally@users.noreply.github.com>
2020-03-09 05:27:44 +0200

    Create Lecture1.py

---
+++ Lecture1.py
@@ -0,0 +1,23 @@
+("Hello World")
+
+"""import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import seaborn as sns"""
+
+
+############################
+#to represent python Keyword  in List 
+
+import keyword
+
+print (keyword.kwlist)
+
+# to represent  length of keyword list in python 
+print(len(keyword.kwlist))
+
+
+
